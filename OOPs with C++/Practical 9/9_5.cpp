be66108694bb076907ae745c2c836c19ff390896
victor1711 <91402503+victor1711@users.noreply.github.com>
2021-11-25 23:19:10 +0530

    Add files via upload

---
+++ OOPs with C++/Practical 9/9_5.cpp
@@ -0,0 +1,19 @@
+// Task-09-05:
+// Sourcecode:
+#include<iostream>
+using namespace std;
+class college
+{
+string name;
+int students;
+};
+struct student
+{
+string name;
+int ns;
+};
+int main()
+{
+struct student s;
+college s2;
+}
