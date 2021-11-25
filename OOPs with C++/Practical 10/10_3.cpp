1e59bec2940feb09efc026a3ae5addb680d81724
victor1711 <91402503+victor1711@users.noreply.github.com>
2021-11-25 23:20:21 +0530

    Add files via upload

---
+++ OOPs with C++/Practical 10/10_3.cpp
@@ -0,0 +1,25 @@
+
+// Task-10-03:
+// Sourcecode:
+#include<iostream>
+using namespace std;
+class number
+{
+int a,b;
+public:
+number(int a,int b)
+{
+this->a=a;
+this->b=b;
+}
+int remainder()
+{
+return (a%b);
+}
+};
+int main()
+{
+
+number n(20,3);
+cout<<"remainder= "<<n.remainder();
+}
